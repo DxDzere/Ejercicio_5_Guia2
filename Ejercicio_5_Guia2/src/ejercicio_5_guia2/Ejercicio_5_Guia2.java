3b8d6f8ecbb409208ab932c9155366c6d9af4c59
DxDzere <74333816+DxDzere@users.noreply.github.com>
2023-03-27 19:53:33 -0300

    Ejercicio Terminado

---
+++ Ejercicio_5_Guia2/src/ejercicio_5_guia2/Ejercicio_5_Guia2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio_5_guia2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Zere
+ */
+public class Ejercicio_5_Guia2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double num;
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println("Ingrese un numero");
+        num = leer.nextDouble();
+        
+        System.out.println("El doble del numero imgresado es: " + num*2);
+        System.out.println("El triple del numero imgresado es: " + num*3);
+        System.out.println("La raiz cuadrada del numero imgresado es: " + Math.sqrt(num));
+    }
+    
+}
